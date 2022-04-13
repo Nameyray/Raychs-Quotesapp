555aae476d7e028332ed7a76de011150f138d0b0
Nameyray <eparis@atlassian.com>
2022-04-13 13:55:37 +0300

    created a class for quotes

--- Raych-Quotesapp/src/app/components/quotes.ts
+++ Raych-Quotesapp/src/app/components/quotes.ts
@@ -1,4 +1,19 @@
 export class Quotes {
+  id: number;
+  name: string;
+  quote: string;
+  author: string;
+  datePosted: Date;
+  upvotes!: number;
+  downvotes!: number;
 
-  
+  constructor(id:number,  name:string, quote:string,  author:string,  datePosted: Date,  upvotes:number,  downvotes:number){
+  this.id = id;
+  this.name = name;
+  this.quote = quote;
+  this.author = author;
+  this.datePosted = datePosted;
+  upvotes= upvotes;
+  downvotes= downvotes;
+  }
 }
